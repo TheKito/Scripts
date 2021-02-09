e15046814c0e15b8767d309dd0fa263f42f5c38c
TheKito <11139894+TheKito@users.noreply.github.com>
2021-02-09 01:29:44 -0300

    Update cloudFlare.sh

--- cloudFlare.sh
+++ cloudFlare.sh
@@ -5,3 +5,10 @@ do
         $IPTABLES -A INPUT  -p tcp --dport 80  -s $ip -j ACCEPT
         $IPTABLES -A INPUT  -p tcp --dport 443 -s $ip -j ACCEPT
 done
+
+IP6TABLES=$(which "ip6tables")
+wget --inet6-only -q https://www.cloudflare.com/ips-v6 -O - | while read ip
+do
+        $IP6TABLES -A INPUT  -p tcp --dport 80  -s $ip -j ACCEPT
+        $IP6TABLES -A INPUT  -p tcp --dport 443 -s $ip -j ACCEPT
+done
