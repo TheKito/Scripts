5a2cae6038ff54f83fa34bf7ba6e1da53ee0c320
TheKito <11139894+TheKito@users.noreply.github.com>
2021-06-24 01:37:11 -0300

    Create Alarm.sh

---
+++ Alarm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#No Changes Alarm
+STR=/Storage/Master/
+ALMTMP=/tmp/ALM
+cat /dev/null > $ALMTMP.err
+cat /dev/null > $ALMTMP.log
+for d in $STR/*; do
+        DEV=$(basename $d)
+        LOG=/tmp/$DEV.log
+        find "$d" -mtime -2 -ls > $LOG
+        [ -s $LOG ] && echo "    OK     $DEV" >> $ALMTMP.log
+        [ -s $LOG ] || echo "   ERROR   $DEV" >> $ALMTMP.err
+done
+cat $ALMTMP.err $ALMTMP.log
+cat $ALMTMP.err $ALMTMP.log | mutt -s "NEW BACKUP ALARM" $(hostname)@$(cat /etc/mailname)
+
+sleep 86400
