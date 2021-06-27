0e8f9516bf19ad79b54e09f54f17c3edff0bb316
TheKito <11139894+TheKito@users.noreply.github.com>
2021-06-27 01:40:37 -0300

    Create GetLinuxBackUpDirectory.sh

---
+++ BackUp/GetLinuxBackUpDirectory.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+HOST=$1
+NAME=$(echo "$HOST" | cut -d "." -f1)
+
+ssh-copy-id $HOST
+ssh $HOST  "apt-get update && apt-get install rsync -y"
+
+SRC=$1:/Data/
+DST=/Storage/Master/$NAME/Data/
+HST=/Storage/History/$(date "+%Y/%m/%d/%H")/$NAME/Data/
+
+echo SRC: $SRC
+echo DST: $DST
+echo HST: $HST
+
+mkdir -p "$DST" && \
+mkdir -p "$HST" && \
+rsync -arv --progress --delete-after --delete -b --backup-dir="$HST" "$SRC" "$DST"
+
+sleep 3600
