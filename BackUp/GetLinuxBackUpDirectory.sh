566b2a5ec0555b057a2c28acd3868d8c8cbdf826
TheKito <11139894+TheKito@users.noreply.github.com>
2021-06-30 13:56:12 -0300

    Update GetLinuxBackUpDirectory.sh

--- BackUp/GetLinuxBackUpDirectory.sh
+++ BackUp/GetLinuxBackUpDirectory.sh
@@ -5,9 +5,9 @@ NAME=$(echo "$HOST" | cut -d "." -f1)
 ssh-copy-id $HOST
 ssh $HOST  "apt-get update && apt-get install rsync -y"
 
-SRC=$1:/Data/
-DST=/Storage/Master/$NAME/Data/
-HST=/Storage/History/$(date "+%Y/%m/%d/%H")/$NAME/Data/
+SRC=$1:/
+DST=/Storage/Master/$NAME/
+HST=/Storage/History/$(date "+%Y/%m/%d/%H")/$NAME/
 
 echo SRC: $SRC
 echo DST: $DST
@@ -15,5 +15,5 @@ echo HST: $HST
 
 mkdir -p "$DST" && \
 mkdir -p "$HST" && \
-rsync -arv --progress --delete-after --delete -b --backup-dir="$HST" "$SRC" "$DST" && \
+rsync -arv --exclude={"/var/lib/mlocate","/var/mail","/var/log","/var/lib/ispell","/var/lib/dpkg","/var/lib/apt","/var/cache","/bin","/boot","/dev","/initrd*","/lib*","/mount","/sbin","/srv","/usr","vmlinuz*","/proc","/sys","/tmp","/run","/mnt","/media","/lost+found","/Storage"} --progress --delete-after --delete -b --backup-dir="$HST" "$SRC" "$DST" && \
 sleep 3600
