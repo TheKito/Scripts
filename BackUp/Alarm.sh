56243283f80e974a9d78a91a3714427b38f9e1b2
TheKito <11139894+TheKito@users.noreply.github.com>
2021-07-06 23:47:45 -0300

    Update Alarm.sh

--- BackUp/Alarm.sh
+++ BackUp/Alarm.sh
@@ -4,14 +4,20 @@ STR=/Storage/Master/
 ALMTMP=/tmp/ALM
 cat /dev/null > $ALMTMP.err
 cat /dev/null > $ALMTMP.log
+cat /dev/null > $ALMTMP.ext
 for d in $STR/*; do
         DEV=$(basename $d)
         LOG=/tmp/$DEV.log
         find "$d" -mtime -2 -ls > $LOG
         [ -s $LOG ] && echo "    OK     $DEV" >> $ALMTMP.log
         [ -s $LOG ] || echo "   ERROR   $DEV" >> $ALMTMP.err
+		echo "" >> $ALMTMP.ext
+		echo "" >> $ALMTMP.ext
+		echo ==== $DEV ==== >> $ALMTMP.ext
+		cat $LOG >> $ALMTMP.ext
+		echo "==========================================================" >> $ALMTMP.ext
 done
-cat $ALMTMP.err $ALMTMP.log
-cat $ALMTMP.err $ALMTMP.log | mutt -s "BackUp Server $(hostname)" $(hostname)@$(cat /etc/mailname)
+cat $ALMTMP.err $ALMTMP.log $ALMTMP.ext
+cat $ALMTMP.err $ALMTMP.log $ALMTMP.ext | mutt -s "BackUp Server $(hostname)" $(hostname)@$(cat /etc/mailname)
 
 sleep 86400
